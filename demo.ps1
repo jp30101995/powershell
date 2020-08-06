6183050ccadbf8c17ed9927246ccebe3a26f8bc0
Jaimin <jp30101995@gmail.com>
2020-08-07 00:29:10 +0530

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -1,10 +1,14 @@
 Write-Host "Congratulations! Your first script executed successfully!!"
 git clone https://github.com/jp30101995/powershell.git
-ls
 cd powershell
-ls
 git status
-git checkout v1
-[System.IO.File]::WriteAllText("./demo2.ps1", "Write-Host 'Congratulations! Your second script executed successfully!!'")
+[System.IO.File]::WriteAllText("./powershell/demo2.ps1", "Write-Host 'Congratulations! Your second script executed successfully!!'")
+
+git checkout v1 --
+git pull origin v1
+git merge master
+git push origin v1
+git checkout master --
+
 git status
 ls
